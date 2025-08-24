9d5ce673a5841937ebf770a27de0a01dfab06d18
ikeasamoahansah <dylanraspino264@gmail.com>
2025-08-24 01:56:53 +0000

    tailwind in control no more sass

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    purge: ["./templates/**/*.html", "./theme/**/*.html"],
-    theme: {},
-    variants: {},
-    plugins: [
-        require('@tailwindcss/typography'),
-    ],
-  };
